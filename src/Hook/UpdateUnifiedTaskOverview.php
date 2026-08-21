255b4778bb27e803a9a93d16d8542c5df29d5aa7
d.savuljesku <d.savuljesku@gmail.com>
2026-08-21 16:06:39 +0200

    Fix setting UTO tasks
    Depends-On: Iff0f1455aadec2e90bca06727050505022032c61
    
    [5.3][galaxy]
    
    ERM49378
    
    Change-Id: Ia070b8798ad7830988f469f3c24615889cdd325b

--- src/Hook/UpdateUnifiedTaskOverview.php
+++ src/Hook/UpdateUnifiedTaskOverview.php
@@ -2,23 +2,55 @@
 
 namespace SimpleTasks\Hook;
 
-use MediaWiki\Extension\UnifiedTaskOverview\ITaskDescriptor;
+use MediaWiki\Extension\UnifiedTaskOverview\TaskStore;
+use MediaWiki\Language\Language;
 use MediaWiki\MediaWikiServices;
-use MediaWiki\Registration\ExtensionRegistry;
-use MediaWiki\User\User;
+use MediaWiki\Title\TitleFactory;
+use SimpleTasks\SimpleTask;
+use SimpleTasks\SimpleTasksTaskDescriptor;
 
-class UpdateUnifiedTaskOverview {
+class UpdateUnifiedTaskOverview implements SimpleTasksPersistTaskHook, SimpleTasksDeleteTaskHook {
 
-	public function onSimpleTasksUpdateTask(
-		ITaskDescriptor $descriptor,
-		User $user,
-		bool $isCompleted
-	): void {
-		if ( !ExtensionRegistry::getInstance()->isLoaded( 'UnifiedTaskOverview' ) ) {
-			return;
-		}
-		MediaWikiServices::getInstance()->getService( 'UnifiedTaskOverview.TaskStore' )
-			->updateTask( $descriptor, $user, $isCompleted );
+	/**
+	 * @param Language $language
+	 * @param TitleFactory $titleFactory
+	 */
+	public function __construct(
+		private readonly Language $language,
+		private readonly TitleFactory $titleFactory
+	) {
 	}
 
+	/**
+	 * @param SimpleTask $task
+	 * @return void
+	 * @throws \Throwable
+	 */
+	public function onSimpleTasksDeleteTask( SimpleTask $task ): void {
+		$descriptor = new SimpleTasksTaskDescriptor( $task, $this->language, $this->titleFactory );
+		$this->getTaskStore()?->deleteTask( $descriptor, $task->getUser() );
+	}
+
+	/**
+	 * @param SimpleTask $task
+	 * @return void
+	 * @throws \Throwable
+	 */
+	public function onSimpleTasksPersistTask( SimpleTask $task ): void {
+		if ( $task->isCompleted() ) {
+			$this->onSimpleTasksDeleteTask( $task );
+			return;
+		}
+		$descriptor = new SimpleTasksTaskDescriptor( $task, $this->language, $this->titleFactory );
+		$this->getTaskStore()?->storeTask( $descriptor, $task->getUser() );
+	}
+
+	/**
+	 * @return TaskStore|null
+	 */
+	private function getTaskStore(): ?TaskStore {
+		$services = MediaWikiServices::getInstance();
+		return $services->hasService( 'UnifiedTaskOverview.TaskStore' ) ?
+			$services->getService( 'UnifiedTaskOverview.TaskStore' ) : null;
+	}
 }
